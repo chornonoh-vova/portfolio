405652e61d4aee3283db842eb2d2f0da194be902
Владимир Черноног <chernonog.vova@gmail.com>
2019-11-27 14:39:30 +0200

    Updates

---
+++ precache-manifest.0652b379d12af86011af08335472cdcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577f85c6981301258dab1f06bc69d209",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c029d50f0f932de904f",
+    "url": "/portfolio/static/css/main.0a3895a7.chunk.css"
+  },
+  {
+    "revision": "982564629918ef5e9a49",
+    "url": "/portfolio/static/js/2.3df688e7.chunk.js"
+  },
+  {
+    "revision": "7c029d50f0f932de904f",
+    "url": "/portfolio/static/js/main.c3377eb3.chunk.js"
+  },
+  {
+    "revision": "e365977b1b661e1b1ec2",
+    "url": "/portfolio/static/js/runtime-main.f0fed0bf.js"
+  },
+  {
+    "revision": "7d7afcd63af014a017253961d0f15bef",
+    "url": "/portfolio/static/media/developer-activity.7d7afcd6.svg"
+  },
+  {
+    "revision": "da307e6a8efacc7775bb5c5258d11433",
+    "url": "/portfolio/static/media/man-on-table.da307e6a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c176db4953c880151204aedd55c5aa88.js"
+  "/portfolio/precache-manifest.0652b379d12af86011af08335472cdcf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
